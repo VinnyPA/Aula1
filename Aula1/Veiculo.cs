42f7f956b41847472fb4a062bf2e9913bca86a1d
curso.ads <curso.ads@LAB06-32>
2022-08-15 21:22:29 -0300

    Adicionar arquivos de projeto.

---
+++ Aula1/Veiculo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1
+{
+    public class Veiculo
+    {
+
+        // declaração de atributos
+        string marca;
+        string modelo;
+        string placa;
+        int ano;
+        // metodo para atribuir valores
+        public void atribuir(string ma, string mo, string pl, int a)
+        {
+
+        }
+    }
+}
